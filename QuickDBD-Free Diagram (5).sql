e8e5fe5e46d1dfd389b46373bdb8126f5e84f8f7
Olusegun Olorunfemi <soolorunfemi@gmail.com>
2020-12-02 09:58:21 -0500

    segs

--- QuickDBD-Free Diagram (5).sql
+++ QuickDBD-Free Diagram (5).sql
@@ -138,10 +138,7 @@ CREATE TABLE "turnout_2000" (
     "State" VARCHAR   NOT NULL,
     "VEP_Total" VARCHAR   NOT NULL,
     "Total_Ballots" INT   NOT NULL,
-    "VEP" INT   NOT NULL,
-    CONSTRAINT "pk_turnout_2000" PRIMARY KEY (
-        "Year"
-     )
+    "VEP" INT   NOT NULL
 );
 
 CREATE TABLE "turnout_2004" (
@@ -149,10 +146,7 @@ CREATE TABLE "turnout_2004" (
     "State" VARCHAR   NOT NULL,
     "VEP_Total" VARCHAR   NOT NULL,
     "Total_Ballots" INT   NOT NULL,
-    "VEP" INT   NOT NULL,
-    CONSTRAINT "pk_turnout_2004" PRIMARY KEY (
-        "Year"
-     )
+    "VEP" INT   NOT NULL
 );
 
 CREATE TABLE "turnout_2008" (
@@ -160,10 +154,7 @@ CREATE TABLE "turnout_2008" (
     "State" VARCHAR   NOT NULL,
     "VEP_Total" VARCHAR   NOT NULL,
     "Total_Ballots" INT   NOT NULL,
-    "VEP" INT   NOT NULL,
-    CONSTRAINT "pk_turnout_2008" PRIMARY KEY (
-        "Year"
-     )
+    "VEP" INT   NOT NULL
 );
 
 CREATE TABLE "turnout_2012" (
@@ -171,10 +162,7 @@ CREATE TABLE "turnout_2012" (
     "State" VARCHAR   NOT NULL,
     "VEP_Total" VARCHAR   NOT NULL,
     "Total_Ballots" INT   NOT NULL,
-    "VEP" INT   NOT NULL,
-    CONSTRAINT "pk_turnout_2012" PRIMARY KEY (
-        "Year"
-     )
+    "VEP" INT   NOT NULL
 );
 
 CREATE TABLE "turnout_2016" (
@@ -182,72 +170,51 @@ CREATE TABLE "turnout_2016" (
     "State" VARCHAR   NOT NULL,
     "VEP_Total" VARCHAR   NOT NULL,
     "Total_Ballots" INT   NOT NULL,
-    "VEP" INT   NOT NULL,
-    CONSTRAINT "pk_turnout_2016" PRIMARY KEY (
-        "Year"
-     )
+    "VEP" INT   NOT NULL
 );
 
-ALTER TABLE "Democrat_2000" ADD CONSTRAINT "fk_Democrat_2000_state_year" FOREIGN KEY("state")
-REFERENCES "Republican_2000" ("state");
+ALTER TABLE "Democrat_2000" ADD CONSTRAINT "fk_Democrat_2000_state" FOREIGN KEY("state")
+REFERENCES "turnout_2000" ("State");
 
 ALTER TABLE "Democrat_2004" ADD CONSTRAINT "fk_Democrat_2004_state" FOREIGN KEY("state")
-REFERENCES "Democrat_2000" ("state");
-
-ALTER TABLE "Democrat_2004" ADD CONSTRAINT "fk_Democrat_2004_year" FOREIGN KEY("state")
 REFERENCES "Republican_2004" ("state");
 
 ALTER TABLE "Democrat_2008" ADD CONSTRAINT "fk_Democrat_2008_state" FOREIGN KEY("state")
-REFERENCES "Democrat_2004" ("state");
-
-ALTER TABLE "Democrat_2008" ADD CONSTRAINT "fk_Democrat_2008_year" FOREIGN KEY("state")
 REFERENCES "Republican_2008" ("state");
 
 ALTER TABLE "Democrat_2012" ADD CONSTRAINT "fk_Democrat_2012_state" FOREIGN KEY("state")
-REFERENCES "Democrat_2008" ("state");
-
-ALTER TABLE "Democrat_2012" ADD CONSTRAINT "fk_Democrat_2012_year" FOREIGN KEY("state")
-REFERENCES "Republican_2012" ("state");
+REFERENCES "turnout_2012" ("State");
 
 ALTER TABLE "Democrat_2016" ADD CONSTRAINT "fk_Democrat_2016_state" FOREIGN KEY("state")
-REFERENCES "Democrat_2012" ("state");
-
-ALTER TABLE "Democrat_2016" ADD CONSTRAINT "fk_Democrat_2016_year" FOREIGN KEY("state")
 REFERENCES "Republican_2016" ("state");
 
-ALTER TABLE "Republican_2000" ADD CONSTRAINT "fk_Republican_2000_state_year" FOREIGN KEY("state", "year")
-REFERENCES "turnout_2000" ("State", "Year");
+ALTER TABLE "Republican_2000" ADD CONSTRAINT "fk_Republican_2000_state" FOREIGN KEY("state")
+REFERENCES "Democrat_2000" ("state");
 
 ALTER TABLE "Republican_2004" ADD CONSTRAINT "fk_Republican_2004_state" FOREIGN KEY("state")
-REFERENCES "Republican_2000" ("state");
-
-ALTER TABLE "Republican_2008" ADD CONSTRAINT "fk_Republican_2008_state" FOREIGN KEY("state")
-REFERENCES "Republican_2004" ("state");
-
-ALTER TABLE "Republican_2012" ADD CONSTRAINT "fk_Republican_2012_state" FOREIGN KEY("state")
-REFERENCES "Republican_2008" ("state");
-
-ALTER TABLE "Republican_2016" ADD CONSTRAINT "fk_Republican_2016_state" FOREIGN KEY("state")
-REFERENCES "Republican_2012" ("state");
-
-ALTER TABLE "turnout_2000" ADD CONSTRAINT "fk_turnout_2000_State" FOREIGN KEY("State")
 REFERENCES "turnout_2004" ("State");
 
-ALTER TABLE "turnout_2004" ADD CONSTRAINT "fk_turnout_2004_Year" FOREIGN KEY("Year")
-REFERENCES "Republican_2004" ("year");
-
-ALTER TABLE "turnout_2004" ADD CONSTRAINT "fk_turnout_2004_State" FOREIGN KEY("State")
+ALTER TABLE "Republican_2008" ADD CONSTRAINT "fk_Republican_2008_state" FOREIGN KEY("state")
 REFERENCES "turnout_2008" ("State");
 
-ALTER TABLE "turnout_2008" ADD CONSTRAINT "fk_turnout_2008_Year" FOREIGN KEY("Year")
-REFERENCES "Republican_2008" ("year");
+ALTER TABLE "Republican_2012" ADD CONSTRAINT "fk_Republican_2012_state" FOREIGN KEY("state")
+REFERENCES "Democrat_2012" ("state");
 
-ALTER TABLE "turnout_2012" ADD CONSTRAINT "fk_turnout_2012_Year" FOREIGN KEY("Year")
-REFERENCES "Republican_2012" ("year");
-
-ALTER TABLE "turnout_2012" ADD CONSTRAINT "fk_turnout_2012_State" FOREIGN KEY("State")
+ALTER TABLE "Republican_2016" ADD CONSTRAINT "fk_Republican_2016_state" FOREIGN KEY("state")
 REFERENCES "turnout_2016" ("State");
 
-ALTER TABLE "turnout_2016" ADD CONSTRAINT "fk_turnout_2016_Year_State" FOREIGN KEY("Year", "State")
-REFERENCES "Republican_2016" ("year", "state");
+ALTER TABLE "turnout_2000" ADD CONSTRAINT "fk_turnout_2000_State" FOREIGN KEY("State")
+REFERENCES "Republican_2000" ("state");
+
+ALTER TABLE "turnout_2004" ADD CONSTRAINT "fk_turnout_2004_State" FOREIGN KEY("State")
+REFERENCES "Democrat_2004" ("state");
+
+ALTER TABLE "turnout_2008" ADD CONSTRAINT "fk_turnout_2008_State" FOREIGN KEY("State")
+REFERENCES "Democrat_2008" ("state");
+
+ALTER TABLE "turnout_2012" ADD CONSTRAINT "fk_turnout_2012_State" FOREIGN KEY("State")
+REFERENCES "Republican_2012" ("state");
+
+ALTER TABLE "turnout_2016" ADD CONSTRAINT "fk_turnout_2016_State" FOREIGN KEY("State")
+REFERENCES "Democrat_2016" ("state");
 
