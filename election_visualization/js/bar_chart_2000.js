04ec75df83f5a4d8a430750a72823d695cb5d5be
Julie Pyle <drjuliepyle@gmail.com>
2020-12-04 12:47:04 -0500

    Added working double bar chart to Visualization 3

--- election_visualization/js/bar_chart_2000.js
+++ election_visualization/js/bar_chart_2000.js
@@ -1,16 +1,17 @@
+
 var container = d3.select('#d3Id'),
-      width = 1300,
-      height = 660,
-      margin = {top: 200, right: 30, bottom: 30, left: 55},
-      barPadding = .2,
-      axisTicks = {qty: 5, outerSize: 0};
+    width = 1300,
+    height = 660,
+    margin = {top: 60, right: 20, bottom: 20, left: 70},
+    barPadding = .2,
+    axisTicks = {qty: 5, outerSize: 0};
 
 var svg = container
-     .append("svg")
-     .attr("width", width)
-     .attr("height", height)
-     .append("g")
-     .attr("transform", `translate(${margin.left},${margin.top})`);
+   .append("svg")
+   .attr("width", width)
+   .attr("height", height)
+   .append("g")
+   .attr("transform", `translate(${margin.left},${margin.top})`);
 
 var xScale0 = d3.scaleBand().range([0, width - margin.left - margin.right]).padding(barPadding)
 
@@ -26,51 +27,50 @@ d3.csv("data/merge_df_2000.csv").then(function(electionData) {
 
     console.log(electionData);
   
-  //   // Cast the candidate votes value to a number for each piece of electionData
+  //   // Cast the votes value to a number for each piece of electionData
     electionData.forEach(function(d) {
       d.candidatevotes_Dem = +d.candidatevotes_Dem;
       d.candidatevotes_Rep = +d.candidatevotes_Rep;
     });
 
+
 xScale0.domain(electionData.map(d => d.state_po_Dem))
 xScale1.domain(['candidatevotes_Dem', 'candidatevotes_Rep']).range([0, xScale0.bandwidth()])
 yScale.domain([0, d3.max(electionData, d => d.candidatevotes_Dem > d.candidatevotes_Rep ? d.candidatevotes_Dem : d.candidatevotes_Rep)])
 
-
-var state_po_Dem = svg.selectAll(".state_po_Dem")
+var state = svg.selectAll(".state_abbr")
   .data(electionData)
   .enter().append("g")
-  .attr("class", "state_po_Dem")
+  .attr("class", "state_abbr")
   .attr("transform", d => `translate(${xScale0(d.state_po_Dem)},0)`);
 
-/* Add field1 bars */
-state_po_Dem.selectAll(".bar.candidatevotes_Dem")
-  .data(d => [d])
-  .enter()
-  .append("rect")
-  .attr("class", "bar field1")
+  /* Add field1 bars */
+state.selectAll(".bar.candidatevotes_Dem")
+.data(d => [d])
+.enter()
+.append("rect")
+.attr("class", "bar candidatevotes_Dem")
 .style("fill","blue")
-  .attr("x", d => xScale1('candidatevotes_Dem'))
-  .attr("y", d => yScale(d.candidatevotes_Dem))
-  .attr("width", xScale1.bandwidth())
-  .attr("height", d => {
-    return height - margin.top - margin.bottom - yScale(d.candidatevotes_Dem)
-  });
-  
-/* Add field2 bars */
-state_po_Dem.selectAll(".bar.candidatevotes_Rep")
-  .data(d => [d])
-  .enter()
-  .append("rect")
-  .attr("class", "bar candidatevotes_Rep")
-.style("fill","red")
-  .attr("x", d => xScale1('candidatevotes_Rep'))
-  .attr("y", d => yScale(d.candidatevotes_Rep))
-  .attr("width", xScale1.bandwidth())
-  .attr("height", d => {
-    return height - margin.top - margin.bottom - yScale(d.candidatevotes_Dem)
-  });
+.attr("x", d => xScale1('candidatevotes_Dem'))
+.attr("y", d => yScale(d.candidatevotes_Dem))
+.attr("width", xScale1.bandwidth())
+.attr("height", d => {
+  return height - margin.top - margin.bottom - yScale(d.candidatevotes_Dem)
+});
 
+/* Add field2 bars */
+state.selectAll(".bar.candidatevotes_Rep")
+.data(d => [d])
+.enter()
+.append("rect")
+.attr("class", "bar candidatevotes_Rep")
+.style("fill","red")
+.attr("x", d => xScale1('candidatevotes_Rep'))
+.attr("y", d => yScale(d.candidatevotes_Rep))
+.attr("width", xScale1.bandwidth())
+.attr("height", d => {
+  return height - margin.top - margin.bottom - yScale(d.candidatevotes_Rep)
+});
 // Add the X Axis
 svg.append("g")
      .attr("class", "x axis")
@@ -80,3 +80,5 @@ svg.append("g")
 svg.append("g")
      .attr("class", "y axis")
      .call(yAxis)});
+
+
