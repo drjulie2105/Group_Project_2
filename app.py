869025c94719990fe207cb7a940603b4a68ec9b1
Jon Zagorski <JonZagorski@gmail.com>
2020-12-10 21:18:37 -0500

    made path for heatmap_2000

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import psycopg2
 import os
+import json
+import pprint
 
 from flask import Flask, jsonify , render_template
 TEMPLATE_DIR = os.path.abspath('templates')
@@ -40,66 +42,75 @@ def vis_3():
 def bio_page():
     return render_template('bio_page.html')
 
+@app.route("/heatmap_2000")
+def heatmap_2000():
+    your_json  ='data\lat_long_2000.geojson'
+    filename = os.path.abspath('data/lat_long_2000.geojson')
 
-@app.route("/api/v1.0/Democrat_2000", methods=['post', 'get'])
-def demo00():  
-    cursor.execute("select array_to_json(array_agg(row_to_json(t))) from (select * from democrat_2000 ) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+    with open(filename) as test_file:
+        data = json.load(test_file)
 
-@app.route("/api/v1.0/Democrat_2004", methods=['post', 'get'])
-def demo04():  
-    cursor.execute("select json_agg(t) FROM (SELECT * from democrat_2004) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+    return data
 
-@app.route("/api/v1.0/Democrat_2008", methods=['post', 'get'])
-def demo08():  
-    cursor.execute("select json_agg(t) FROM (SELECT * from democrat_2008) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+# @app.route("/api/v1.0/Democrat_2000", methods=['post', 'get'])
+# def demo00():  
+#     cursor.execute("select array_to_json(array_agg(row_to_json(t))) from (select * from democrat_2000 ) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
 
-@app.route("/api/v1.0/Democrat_2012", methods=['post', 'get'])
-def demo12():  
-    cursor.execute("select json_agg(t) FROM (SELECT * from democrat_2012) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+# @app.route("/api/v1.0/Democrat_2004", methods=['post', 'get'])
+# def demo04():  
+#     cursor.execute("select json_agg(t) FROM (SELECT * from democrat_2004) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
 
-@app.route("/api/v1.0/Democrat_2016", methods=['post', 'get'])
-def demo16():  
-    cursor.execute("select json_agg(t) FROM (SELECT * from democrat_2016) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+# @app.route("/api/v1.0/Democrat_2008", methods=['post', 'get'])
+# def demo08():  
+#     cursor.execute("select json_agg(t) FROM (SELECT * from democrat_2008) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
 
-@app.route("/api/v.10/Republican_2000")
-def rep00():
-    cursor.execute("select json_agg(t) FROM (SELECT * from republican_2000) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+# @app.route("/api/v1.0/Democrat_2012", methods=['post', 'get'])
+# def demo12():  
+#     cursor.execute("select json_agg(t) FROM (SELECT * from democrat_2012) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
 
-@app.route("/api/v.10/Republican_2004")
-def rep04():
-    cursor.execute("select json_agg(t) FROM (SELECT * from republican_2004) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+# @app.route("/api/v1.0/Democrat_2016", methods=['post', 'get'])
+# def demo16():  
+#     cursor.execute("select json_agg(t) FROM (SELECT * from democrat_2016) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
 
-@app.route("/api/v.10/Republican_2008")
-def rep08():
-    cursor.execute("select json_agg(t) FROM (SELECT * from republican_2008) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+# @app.route("/api/v.10/Republican_2000")
+# def rep00():
+#     cursor.execute("select json_agg(t) FROM (SELECT * from republican_2000) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
 
-@app.route("/api/v.10/Republican_2012")
-def rep12():
-    cursor.execute("select json_agg(t) FROM (SELECT * from republican_2012) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+# @app.route("/api/v.10/Republican_2004")
+# def rep04():
+#     cursor.execute("select json_agg(t) FROM (SELECT * from republican_2004) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
 
-@app.route("/api/v.10/Republican_2016")
-def rep16():
-    cursor.execute("select json_agg(t) FROM (SELECT * from republican_2016) t")
-    result = cursor.fetchall()
-    return jsonify(result)
+# @app.route("/api/v.10/Republican_2008")
+# def rep08():
+#     cursor.execute("select json_agg(t) FROM (SELECT * from republican_2008) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
+
+# @app.route("/api/v.10/Republican_2012")
+# def rep12():
+#     cursor.execute("select json_agg(t) FROM (SELECT * from republican_2012) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
+
+# @app.route("/api/v.10/Republican_2016")
+# def rep16():
+#     cursor.execute("select json_agg(t) FROM (SELECT * from republican_2016) t")
+#     result = cursor.fetchall()
+#     return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
